398c9fcd11f3783328b4a704ce56207ed7439cd6
Gen <gen@rose.dev>
2024-05-06 07:35:47 -0700

    Add head turning

--- sit-mod/main.js
+++ sit-mod/main.js
@@ -1,26 +1,15 @@
-
-
-/*
-name: "sitFaceDownLeft" < Face left while sitting
-name: "sitFaceUpRight" < Face right while sitting
-I have no idea why the names are like this, the other poses don't work?
-*/
 // DEBUG STUFF
 var debugMode = true;
-var oldColUpdate = null; // store old collision update function to restore
+var oldColUpdate = null;
 
 var isSitting = false;
-ig.input.bind(219, 'doSitLea');
-//ig.game.playerEntity.getAlignedPos()
-//{x: 1007.69, y: 475.54, z: 0}
+var lastPressedAdjustedHeadKeyCache = null;
+ig.input.bind(220, 'doSitLea');
+ig.input.bind(219, 'doSitLeaLeft');
+ig.input.bind(221, 'doSitLeaRight');
 ig.ENTITY.Player.inject({
     update(...args) {
         if (isSitting) {
-            // if sitting, change head direction with left and right arrow keys, 32 is spacebar, left arrow is 37, right arrow is 39
-            /*
-                            ig.game.playerEntity.getAlignedPos()
-            
-            {x: 867.89, y: 659.28, z: 0}*/
             /*
             let pos = ig.game.playerEntity.getAlignedPos()
                 pos.x -= 8
@@ -57,53 +46,29 @@ ig.ENTITY.Player.inject({
                 ig.game.playerEntity.setPos(pos.x, pos.y, pos.z)
             }
 
+            if (ig.input.pressed('doSitLeaLeft')) {
+                if (lastPressedAdjustedHeadKeyCache === "sitFaceDownLeft") {
+                    doSitAction("sit");
+                    lastPressedAdjustedHeadKeyCache = "sit";
+                } else {
+                    doSitAction("sitFaceDownLeft");
+                    lastPressedAdjustedHeadKeyCache = "sitFaceDownLeft";
+                }
+            } else if (ig.input.pressed('doSitLeaRight')) {
+                if (lastPressedAdjustedHeadKeyCache === "sitFaceUpRight") {
+                    doSitAction("sit");
+                    lastPressedAdjustedHeadKeyCache = "sit";
+                } else {
+                    doSitAction("sitFaceUpRight");
+                    lastPressedAdjustedHeadKeyCache = "sitFaceUpRight";
+                }
+            }
         }
 
         if (ig.input.pressed('doSitLea')) {
 
             if (!isSitting) {
-                new cc.ig.events.DO_ACTION({
-                    entity: {
-                        player: true
-                    },
-                    action: [
-                        {
-                            type: "SHOW_EXTERN_ANIM",
-                            anim: {
-                                sheet: "player-poses",
-                                //name: "sitFaceDownLeft"
-                                name: Math.random() > 0.5 ? "sitFaceDownLeft" : "sitFaceUpRight"
-                            }
-                        },
-                        {
-                            value: 0.03,
-                            type: "SET_RELATIVE_SPEED"
-                        },
-                        {
-                            value: false,
-                            type: "SET_JUMPING"
-                        },
-                        {
-                            name: "loop",
-                            type: "LABEL"
-                        },
-                        {
-                            time: 1,
-                            rotateSpeed: 10,
-                            stopBeforeEdge: false,
-                            allowStop: true,
-                            type: "PLAYER_MOVE_TO_DIR"
-                        },
-                        {
-                            name: "loop",
-                            type: "GOTO_LABEL"
-                        }
-                    ],
-                    repeating: false,
-                    wait: false,
-                    keepState: false,
-                    immediately: false
-                }).start(cc.ig.playerInstance())
+                doSitAction();
                 isSitting = true;
                 ig.game.playerEntity.coll.shadow.size = 0;
                 // Old method using player movement, seemed buggy and offsets player every time we set this
@@ -128,5 +93,55 @@ ig.ENTITY.Player.inject({
     },
 });
 
+function doSitAction(sitDir = "sit") {
+    new cc.ig.events.DO_ACTION({
+        entity: {
+            player: true
+        },
+        action: [
+            {
+                type: "SHOW_EXTERN_ANIM",
+                anim: {
+                    sheet: "player-poses",
+                    //name: "sitFaceDownLeft"
+                    name: sitDir
+                    /*
+name: "sit" < Sitting pose
+/*name: "sitFaceDownLeft" < Face left while sitting
+name: "sitFaceUpRight" < Face right while sitting
+I have no idea why the names are like this, the other poses don't work?
+*/
 
+                }
+            },
+            {
+                value: 0.03,
+                type: "SET_RELATIVE_SPEED"
+            },
+            {
+                value: false,
+                type: "SET_JUMPING"
+            },
+            {
+                name: "loop",
+                type: "LABEL"
+            },
+            {
+                time: 1,
+                rotateSpeed: 10,
+                stopBeforeEdge: false,
+                allowStop: true,
+                type: "PLAYER_MOVE_TO_DIR"
+            },
+            {
+                name: "loop",
+                type: "GOTO_LABEL"
+            }
+        ],
+        repeating: false,
+        wait: false,
+        keepState: false,
+        immediately: false
+    }).start(cc.ig.playerInstance())
+}
 
